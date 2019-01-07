d6e11d9d4c13ca6a0908ac9e65cd4ac9df932e58
Ryenoceros <35402708+Ryenoceros@users.noreply.github.com>
2019-01-07 08:22:47 +0800

    update

--- Haliya-2D/Assets/Scripts/Movement.cs
+++ Haliya-2D/Assets/Scripts/Movement.cs
@@ -4,16 +4,46 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+	public float maxSpeed = 3;
+	public float speed = 50f;
+	public float jumpPower = 150f;
+
+	public bool grounded;
+
+	private Rigidbody2D rb2d;
+	private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
-        transform.position = transform.position + horizontal * Time.deltaTime;
+        //Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        //transform.position = transform.position + horizontal * Time.deltaTime;
+
+        anim.SetBool("Grounded",grounded);
+        anim.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+    }
+
+    void FixedUpdate()
+    {
+    	float h = Input.GetAxis("Horizontal");
+
+    	//Moving the player
+    	rb2d.AddForce((Vector2.right * speed) * h);
+
+    	//Limiting the speed of the player
+    	if(rb2d.velocity.x > maxSpeed)
+    	{
+    		rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
+    	}
+
+    	if (rb2d,velocity.x < -maxSpeed){
+    		rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);
+    	}
     }
 }
