abdfe4861c77ce11383637bd55660ec65eea5fed
RyanDev <35402708+Ryenoceros@users.noreply.github.com>
2019-01-14 17:07:55 +0800

    1-14

---
+++ Haliya-2D/Assets/Scripts/Hud.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hud : MonoBehaviour
+{
+    public Sprite[] HeartSprites;
+
+    public Image HeartUI;
+
+    private Movement player;
+
+    void Start(){
+    	player = GameObject.FindGameObjectWithTag("Player").GetComponent<Movement>();
+
+
+    }
+
+    void Update(){
+    	HeartUI.sprite = HeartSprites[player.curHealth];
+    }
+}
