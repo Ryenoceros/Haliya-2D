09cf74ccdbd07917d50337fc56d3b9ee72052522
RyanDev <35402708+Ryenoceros@users.noreply.github.com>
2019-01-15 16:34:22 +0800

    1-15-2018

---
+++ Haliya-2D/Assets/Scripts/Spikes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour{
+
+	private Movement player;
+
+	void Start(){
+
+		player = GameObject.FindGameObjectWithTag("Player").GetComponent<Movement>();
+
+	}
+
+	void OnTriggerEnter2D(Collider2D ool) {
+
+		if (ool.CompareTag("Player")){
+
+			player.Damage(1);
+
+			StartCoroutine (player.Knockback(0.02f, 350, player.transform.position ));
+		}
+
+	}
+}
