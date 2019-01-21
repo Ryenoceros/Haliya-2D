f231b57747629c3e6a655fa095736e1649e8e864
Ryenoceros <35402708+Ryenoceros@users.noreply.github.com>
2019-01-21 19:16:44 +0800

    update

---
+++ Haliya-2D/Assets/Scripts/scrollingBG.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollingBG : MonoBehaviour
+{	
+	public float backgroundSize;
+
+	private Transform cameraTransform;
+	private Transform[] layers;
+	private float viewZone = 10;
+	private int leftIndex;
+	private int rightIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        layers = new Transform[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        	layers[i] = transform.GetChild(i);
+
+        leftIndex = 0;
+        rightIndex = layers.Length-1;
+    }
+
+    public void ScrollLeft(){
+    	int lastRight=rightIndex;
+    	layers[rightIndex].position = Vector3.right * (layers[leftIndex].position.x - backgroundSize);
+    	leftIndex = rightIndex;
+    	rightIndex--;
+    	if(rightIndex < 0)
+    		rightIndex = layers.Length-1;
+    }
+
+    public void ScrollRight(){
+    	int lastLeft=leftIndex;
+    	layers[leftIndex].position = Vector3.right * (layers[rightIndex].position.x + backgroundSize);
+    	rightIndex = leftIndex;
+    	leftIndex++;
+    	if(leftIndex == 0)
+    		leftIndex = 0;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	
+    }
+}
